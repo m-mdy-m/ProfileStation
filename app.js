f059b038323d52d760f45d5e1b73fd3a3a3898fd
mahdi <mahdimamashli1383@gmail.com>
2023-12-07 18:12:51 +0330

    create router project

--- app.js
+++ app.js
@@ -1,28 +1,22 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-
+const path = require('path')
+const routerDir = require("./views/util/path.js")
 const app = express()
-
+const signInRouter = require("./src/routes/signIn.js")
+const signUpRouter = require("./src/routes/signUp.js")
+const profileRouter = require("./src/routes/profiles.js")
+const Err404Router = require("./src/routes/404.js")
 app.set('views engine', 'pug')
 app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({extended : false}))
+app.use(express.static(path.join(routerDir,'public')))
 
-app.get('/', (req,res,next)=>[
-    res.render('signIn', {title : 'home'})
-])
-app.post('/signUp', (req,res,next)=>{
-    res.render('signUp', {title : "signUp"})
-    res.redirect('/profiles')
+app.use("/",(req,res)=>{
+    res.send('test')
 })
-app.post("/signIn",(req,res,next)=>{
-    res.render('signIn' , {title : "signIn"})
-    res.redirect('/profiles')
-    
-})
-app.post("/profiles", (req,res,next)=>{
-    res.render("profiles", {title: "profile user"})
-})
+
 app.listen(3000,()=>{
     console.log("server run on port 3000");
 })
