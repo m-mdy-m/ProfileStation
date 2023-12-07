52b770dc0ff0436b891e83cf989af01f976afeff
mahdi <mahdimamashli1383@gmail.com>
2023-12-08 02:41:35 +0330

    dynamic profiles

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,8 +1,15 @@
-const express = require('express')
-const router = express.Router()
-const path = require("path")
-const pathRouter = require("../../views/util/path.js")
-router.get("/", (req,res)=>{
-    res.render('home', {title : "home", path:'/'})
-})
-module.exports = router
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+const pathRouter = require("../../views/util/path.js");
+router.get("/", (req, res) => {
+  res.render("home", {
+    title: "home",
+    path: "/",
+    username: "m__mdy__m",
+    name: "mahdi",
+    age: "20",
+    family: "mamashlis",
+  });
+});
+module.exports = router;
