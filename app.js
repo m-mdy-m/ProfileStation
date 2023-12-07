99a5aeec720946e0da4e4634e8a301174ec69a8d
mahdi <mahdimamashli1383@gmail.com>
2023-12-07 18:01:40 +0330

    update pug files and css main

--- app.js
+++ app.js
@@ -8,17 +8,20 @@ app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({extended : false}))
 
+app.get('/', (req,res,next)=>[
+    res.render('signIn', {title : 'home'})
+])
 app.post('/signUp', (req,res,next)=>{
-    res.render('')
+    res.render('signUp', {title : "signUp"})
     res.redirect('/profiles')
 })
-app.post("/signIn",(res,res,next)=>{
-    res.render('')
+app.post("/signIn",(req,res,next)=>{
+    res.render('signIn' , {title : "signIn"})
     res.redirect('/profiles')
     
 })
-app.get("/profiles", (res,req,next)=>{
-
+app.post("/profiles", (req,res,next)=>{
+    res.render("profiles", {title: "profile user"})
 })
 app.listen(3000,()=>{
     console.log("server run on port 3000");
