670ba5c78ac3d34b4ef22e986a450fd7b69ade46
mahdi <mahdimamashli1383@gmail.com>
2023-12-08 01:02:35 +0330

    create search

---
+++ public/js/header.js
@@ -0,0 +1,12 @@
+let searchImg = document.querySelector('.header__User-search-img')
+let searchInput = document.querySelector('.header__User-search-input')
+let body = document.body
+searchImg.addEventListener('click',(e)=>{
+    e.stopPropagation()
+    searchInput.classList.toggle('showInput');
+})
+body.addEventListener('click', (e) => {
+    if (!searchInput.contains(e.target) && e.target !== searchImg) {
+        searchInput.classList.remove('showInput');
+    }
+});
