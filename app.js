fa931873f97584fc7d29934e68537f375f3663f2
mahdi <mahdimamashli1383@gmail.com>
2023-12-08 12:59:41 +0330

    create template 404 and home

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const home = require("./src/routes/home.js");
 const signInRouter = require("./src/routes/signIn.js");
 const signUpRouter = require("./src/routes/signUp.js");
 const profileRouter = require("./src/routes/profiles.js");
-const Err404Router = require("./src/routes/404.js");
 
 app.set("view engine", "pug");
 app.set("views", "views");
@@ -15,11 +14,18 @@ app.set("views", "views");
 console.log(path.join(__dirname, "public"));
 app.use(home);
 app.use(signInRouter);
-app.use(signUpRouter)
-app.use(Err404Router)
+app.use(signUpRouter);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use((req, res, next) => {
+  res.status(404).render("404", {
+    title: "404 - Page Not Found",
+    username: "m__mdy__m",
+    name: "mahdi",
+    age: "20",
+    family: "mamashlis",
+  });
+});
 app.listen(3000, () => {
   console.log("server run on port 3000");
 });
